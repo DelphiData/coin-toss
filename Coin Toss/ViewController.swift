d23c5ae1eb87ffde4fd0160f3299744dc90f56ab
DelphiData <ljbarg0@Lonnies-MacBook-Air.local>
2015-02-04 21:09:39 -0500

    The iPhone app is now functioning - will improve graphical interface

--- Coin Toss/ViewController.swift
+++ Coin Toss/ViewController.swift
@@ -9,7 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var coinIma: UIImageView!
+    @IBAction func phoneFlip(sender: AnyObject) {
+        var randomNumber = arc4random_uniform(2)
+        let frame1 = UIImage(named: "Heads.png")
+        let frame2 = UIImage(named: "This Tails.png")
+        
+        if randomNumber == 0 {
+          coinIma.image = frame1
+            
+        }
+        else {
+            coinIma.image = frame2
+        }
+        
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
